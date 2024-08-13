a4dcea6e3930bf3655ef21dc43bf590d4f238a98
Dineshyadav3505 <dineshyadav20001210@gmail.com>
2024-08-13 16:42:19 +0530

    Add @tabler/icons-react package and update CoursesSection component

--- src/components/CoursesSection.tsx
+++ src/components/CoursesSection.tsx
@@ -1,10 +1,21 @@
 "use client";
 import React, { useState, useRef, useEffect } from "react";
 import { SparklesCore } from "./ui/sparkles";
+import { BentoGrid, BentoGridItem } from "./ui/bento-grid";
+import { cn } from "@/lib/utils";
+import {
+  IconArrowWaveRightUp,
+  IconBoxAlignRightFilled,
+  IconBoxAlignTopLeft,
+  IconClipboardCopy,
+  IconFileBroken,
+  IconSignature,
+  IconTableColumn,
+} from "@tabler/icons-react";
 
 const CoursesSection = () => {
   return (
-    <div className="text-white pt-[48rem] relative bg-transparent">
+    <div className="text-white pt-[48rem] relative bg-transparent ">
         <div className="h-[20rem] w-full bg-black flex flex-col items-center  overflow-hidden rounded-t-3xl">
           <h1 className="md:text-3xl text-3xl mt-10 lg:text-7xl font-bold text-center text-white relative z-20">
             Harmony Institute
@@ -33,11 +44,71 @@ const CoursesSection = () => {
 
         </div>
 
-        <div className="min-h-screen bg-black">
-          chachc
+        <div className="min-h-screen bg-black pb-24">
+          <BentoGrid className="max-w-4xl mx-auto">
+          {items.map((item, i) => (
+            <BentoGridItem
+              key={i}
+              title={item.title}
+              description={item.description}
+              header={item.header}
+              icon={item.icon}
+              className={i === 3 || i === 6 ? "md:col-span-2" : ""}
+          />
+      ))}
+    </BentoGrid>
+          
         </div>
      </div>
   )
 }
 
 export default CoursesSection;
+
+
+
+const items = [
+  {
+    title: "Music Theory Fundamentals",
+    description: "Master the building blocks of music and learn to read sheet music.",
+    header: "<Skeleton />",
+    icon: <IconClipboardCopy className="h-4 w-4 text-neutral-500" />,
+  },
+  {
+    title: "The Art of Songwriting",
+    description: "Explore the creative process of crafting memorable songs.",
+    header: "<Skeleton />",
+    icon: <IconFileBroken className="h-4 w-4 text-neutral-500" />,
+  },
+  {
+    title: "Music Production Essentials",
+    description: "Learn the techniques and tools to create professional-sounding recordings.",
+    header: "<Skeleton />",
+    icon: <IconSignature className="h-4 w-4 text-neutral-500" />,
+  },
+  {
+    title: "The History of Rock Music",
+    description:
+      "Dive into the evolution of rock music and its cultural impact.",
+    header: "<Skeleton />",
+    icon: <IconTableColumn className="h-4 w-4 text-neutral-500" />,
+  },
+  {
+    title: "Music Entrepreneurship",
+    description: "Explore the business side of the music industry and learn to thrive as an independent artist.",
+    header: "<Skeleton />",
+    icon: <IconArrowWaveRightUp className="h-4 w-4 text-neutral-500" />,
+  },
+  {
+    title: "Music and the Brain",
+    description: "Discover the fascinating relationship between music and human cognition.",
+    header: "<Skeleton />",
+    icon: <IconBoxAlignTopLeft className="h-4 w-4 text-neutral-500" />,
+  },
+  {
+    title: "World Music Traditions",
+    description: "Explore the rich diversity of musical styles from around the globe.",
+    header:"<Skeleton />",
+    icon: <IconBoxAlignRightFilled className="h-4 w-4 text-neutral-500" />,
+  },
+];
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,47 @@
+import Link from 'next/link'
 import React from 'react'
 
+
 const Footer = () => {
+    const data = [
+        {title:"Explore", link1:"/", lable1:"Home", link2:"/about", lable2:"About", link3:"/contact", lable3:"contact", link4:"/courses", lable4:"our Courses"},
+        {title:"Follow", link1:"/", lable1:"Twitter", link2:"/", lable2:"Instagram", link3:"/", lable3:"facebook",  },
+    ]
+
   return (
-    <div className='text-white relative bg-black'>
+    <div className='text-white relative h-auto lg:flex border-t-[.2px] bg-black'>
+        <div className=" w-full lg:w-[35%] border-r-[.2px] flex justify-center items-center">
+            <h1 className='text-white text-3xl py-10'>Harmony Institute</h1>
+        </div> 
+        <div className="flex flex-col lg:flex-row w-full lg:w-[75%]">
+            {data.map((item, index)=>(
+                <div className="w-[40%] flex flex-col pl-10 py-5">
+                    <h6 className='text-sm capitalize text-zinc-600'>{item.title}</h6>
+                    <Link href={`${item.link1}`} className='text-white capitalize text-base pt-3 py-1'>{item.lable1}</Link>
+                    <Link href={`${item.link2}`} className='text-white capitalize text-base py-1'>{item.lable2}</Link>
+                    <Link href={`${item.link3}`} className='text-white capitalize text-base py-1'>{item.lable3}</Link>
+                    <Link href={`${item.link4}`} className='text-white capitalize text-base py-1'>{item.lable4}</Link>
+                </div>
+            ))}
+            <div className=" w-full h-10 lg:w-[60%] flex px-10 lg:px-3 flex-col py-5">
+            <h6 className='text-sm capitalize text-zinc-600'>join us </h6>
+            
+            <form action="" className='flex bg-black  pb-10 gap-3 w-full' >
+                <input
+                className=' bg-transparent focus:outline-none border-b-[.2px] w-64 px-1 py-3 border-white text-white' 
+                type="email" 
+                />
+
+                <button type='submit' className=' hover:pl-3 duration-500'>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-4">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M13.5 4.5 21 12m0 0-7.5 7.5M21 12H3" />
+                    </svg>
+                </button>
+
+            </form>
+
+            </div>
+        </div> 
         
     </div>
   )
