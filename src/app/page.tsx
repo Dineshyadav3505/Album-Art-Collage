042333fcff072939e1d707e53ac6d1189a73177f
Dineshyadav3505 <dineshyadav20001210@gmail.com>
2024-08-13 15:09:34 +0530

    Add tsparticles dependencies and update page layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
 import HeroSection from '@/components/HeroSection'
+import CoursesSection from '@/components/CoursesSection'
 import React from 'react'
+import Footer from '@/components/Footer'
 
 const page = () => {
   return (
     <div className="min-h-screen bg-back/[.96] antialiased bg-grid-white/[0.2]">
       <HeroSection/>
+      <CoursesSection />
+      <Footer/>
   
 
     </div>
--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,23 +1,28 @@
 import Link from "next/link"
 import { Spotlight } from "./ui/Spotlight"
 import { HoverBorderGradient } from "./ui/hover-border-gradient";
+import { TextGenerateEffect } from "./ui/text-generate-effect";
 
 function HeroSection() {
+
+    const words = `Welcome to Harmony Institute, where we believe that every aspiring musician deserves the opportunity to shine. Our comprehensive programs are designed to nurture talent and foster creativity, offering a diverse range of courses in performance, composition, music theory, and technology.`
   return (
     <div
-    className="h-auto md:h-[45rem] w-full rounded-md flex flex-col items-center justify-center relative overflow-hidden mx-auto py-10 md:py-0"
+    className="h-auto lg:h-screen bg-zinc-900 fixed md:h-[45rem] w-full rounded-md flex flex-col items-center justify-center  overflow-hidden mx-auto py-10 md:py-0"
     >
         <Spotlight
             className="-top-40 left-0 md:left-60 md:-top-20"
             fill="white"
         />
-        <div className="p-4 relative z-10 w-full text-center" >
+        <div className="p-4 relative  w-full text-center" >
             <h1
             className="mt-20 md:mt-0 text-4xl md:text-7xl font-bold bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400"
             >Unleash Your Musical Potential</h1>
             <p
             className="mt-10 font-normal text-base md:text-lg text-neutral-300 px-5 lg:px-44 mx-auto"
-            >Welcome to <span className=" font-semibold">Harmony Institute</span>, where we believe that every aspiring musician deserves the opportunity to shine. Our comprehensive programs are designed to nurture talent and foster creativity, offering a diverse range of courses in performance, composition, music theory, and technology.</p>
+            >
+                <TextGenerateEffect words={words} />
+            </p>
 
             <div className="mt-10 flex justify-center">
                 <Link href={"/courses"}>
